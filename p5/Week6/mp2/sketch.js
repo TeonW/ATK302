4b077b22cf0ad71cfb5397d3e533c9ee7fa030b4
TeonW <54369162+TeonW@users.noreply.github.com>
2020-02-20 10:08:13 -0600

    5

---
+++ p5/Week6/mp2/sketch.js
@@ -0,0 +1,56 @@
+var myState = 0;
+var timer = 0 ;
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+}
+
+function draw() {
+  // put drawing code here
+
+
+  switch (myState) {
+    case 0:
+      background('gray');
+      textSize(15)
+      text("Who do I think is going to win the championship", 100, 100);
+
+      timer = timer + 1
+      if (timer > 200) {
+        myState = 1 ;
+        timer = 0 ;
+      }
+      break;
+
+    case 1:
+    background('yellow') ;
+    text("I think LeBron is winning a championship", 100, 100) ;
+      break;
+
+    case 2:
+    background('purple') ;
+    text("He's going to do it for MAMBA and GIGI", 100, 100) ;
+      break;
+
+    case 3:
+    background('blue') ;
+    text("Wait until the playoffs, he's a different animal but the same beast", 25, 100) ;
+      break;
+
+    case 4:
+    background('red') ;
+    text("NBA is where amazing happens and he's been nothing short this season", 25, 100) ;
+      break;
+
+  }
+
+}
+
+
+
+function mouseReleased() {
+  myState = myState + 1;
+  if (myState > 4) {
+    myState = 0;
+  }
+}
